e81d43bf4250bf2dd5fd7e582840996b94a396d9
Tanishk Shrivastava <tanishkshrivastava0802@gmail.com>
2026-05-22 09:08:38 +0530

    The changes made here header/footer ejs partials, login/signup/createPost ejs pages, createPostSylte CSS file

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ import express from "express"
 
 const app = express();
 const port = 3000;
-
 app.use(express.static("public"))
 
+var blogArray = [ {
+        title: "What is Blog?",
+        content: "Check the full article inside.",
+        image: "/Images/BlogImage1.png"
+    },
+
+    {
+        title: "The problem with Indian Wildlife Tourism",
+        content: "Check the full article inside.",
+        image: "/Images/BlogImage2.png"
+    },
+
+    {
+        title: "What Einstein said about Gravity",
+        content: "Check the full article inside.",
+        image : "/Images/BlogImage3.png"
+    }
+
+];
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs",{blogArray});
 })
 
 app.post("/article",(req,res)=>{
     res.render("")
+})
+
+app.get("/create",(req,res)=>{
+    res.render("createPost.ejs")
 })
 
 app.listen(port, ()=>{
